0f9c08e8e6f7893a7d12672a7c932690981ff686
sebastianusbara <sebastianus.bara@gmail.com>
2015-12-17 17:00:26 +0700

    Pie Chart

--- assets/js/main.min.js
+++ assets/js/main.min.js
@@ -10,7 +10,8 @@
     var assets = {
         _jquery_cdn     : 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js',
         _jquery_local   : path.js + 'jquery.min.js',
-        _fastclick      : path.js + 'fastclick.min.js'
+        _fastclick      : path.js + 'fastclick.min.js',
+        _highchart      : path.js + 'highcharts.min.js'
     };
 
     var Site = {
@@ -19,6 +20,7 @@
             Site.fastClick();
             Site.enableActiveStateMobile();
             Site.WPViewportFix();
+            Site.highChart();
 
             window.Site = Site;
         },
@@ -46,8 +48,59 @@
                 style.appendChild(fix);
                 document.getElementsByTagName('head')[0].appendChild(style);
             }
-        }
+        },
 
+        highChart: function () {
+            var $graph  = $('.graph');
+            var $chart  = $('#container');
+            var $pie    = $('#pie');
+
+            if ( !$chart.length ) return;
+            if ( !$pie.length ) return;
+
+            Modernizr.load( {
+                load    : assets._highchart,
+                complete: initChart
+            });
+
+            function initChart() {
+
+                var data        = $chart.attr('data-movies');
+                var title       = $chart.attr('data-title');
+                var pie         = $pie.attr('data-segment');
+                var pieTitle    = $pie.attr('data-pieTitle');
+
+                $.getJSON( data, function(json, textStatus) {
+                    $chart.highcharts({
+                        chart: {
+                            type: 'line'
+                        },
+                        title: {
+                            text: title
+                        },
+                        xAxis: json.month,
+                        yAxis: json.legend,
+                        series: json.viewer
+                    });
+                });
+
+                $.getJSON( pie, function(json, textStatus) {
+                    $pie.highcharts({
+                        chart: {
+                            type: 'pie'
+                        },
+                        title: {
+                            text: pieTitle
+                        },
+                        series: json.segment
+                    });
+                });
+
+                $graph.ready(function() {
+                    $graph.removeClass('load');
+                });
+            }
+        }
     };
 
     var checkJquery = function () {
